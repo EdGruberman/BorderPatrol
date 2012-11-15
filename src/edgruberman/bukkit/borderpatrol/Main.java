105a6832d08c64b4db8b0bde267c7be73f38d208
EdGruberman <ed@rjump.com>
2012-11-14 23:17:19 -0700

    FIX: Configuration file attempted to be extracted when already exists

--- src/edgruberman/bukkit/borderpatrol/Main.java
+++ src/edgruberman/bukkit/borderpatrol/Main.java
@@ -21,7 +21,7 @@ public final class Main extends CustomPlugin {
 
     @Override
     public void onLoad() {
-        this.putConfigMinimum("config.yml", "2.4.0");
+        this.putConfigMinimum(CustomPlugin.CONFIGURATION_FILE, "2.4.0");
         this.putConfigMinimum("safety.yml", "2.4.3");
     }
 
