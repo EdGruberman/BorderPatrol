c3d769908d3f67644a4ab2110a5dc91a36c11ed0
EdGruberman <ed@rjump.com>
2013-07-09 23:45:56 -0700

    Update build process Added support for 1.6.2 dev Removed support for 1.4.6 dev, 1.5 dev and 1.5.1 beta

--- src/edgruberman/bukkit/borderpatrol/craftbukkit/CraftBukkit_v1_6_R2.java
+++ src/edgruberman/bukkit/borderpatrol/craftbukkit/CraftBukkit_v1_6_R2.java
@@ -1,11 +1,11 @@
 package edgruberman.bukkit.borderpatrol.craftbukkit;
 
-import net.minecraft.server.v1_5_R2.WorldServer;
+import net.minecraft.server.v1_6_R2.WorldServer;
 
 import org.bukkit.World;
-import org.bukkit.craftbukkit.v1_5_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_6_R2.CraftWorld;
 
-public class CraftBukkit_v1_5_R2 extends CraftBukkit {
+public class CraftBukkit_v1_6_R2 extends CraftBukkit {
 
     @Override
     public boolean getForceChunkLoad(final World world) {
@@ -21,7 +21,7 @@ public class CraftBukkit_v1_5_R2 extends CraftBukkit {
 
     @Override
     public boolean isBuildable(final World world, final int x, final int y, final int z) {
-        final net.minecraft.server.v1_5_R2.World nmsWorld = ((CraftWorld) world).getHandle();
+        final net.minecraft.server.v1_6_R2.World nmsWorld = ((CraftWorld) world).getHandle();
         return nmsWorld.getMaterial(x, y, z).isBuildable();
     }
 
