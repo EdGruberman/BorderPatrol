537ae5767d157c0e452198875a79aa27d698d404
EdGruberman <ed@rjump.com>
2013-09-22 12:10:43 -0700

    add support for CraftBukkit 1.6.4

---
+++ src/edgruberman/bukkit/borderpatrol/craftbukkit/CraftBukkit_v1_6_R3.java
@@ -0,0 +1,28 @@
+package edgruberman.bukkit.borderpatrol.craftbukkit;
+
+import net.minecraft.server.v1_6_R3.WorldServer;
+
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_6_R3.CraftWorld;
+
+public class CraftBukkit_v1_6_R3 extends CraftBukkit {
+
+    @Override
+    public boolean getForceChunkLoad(final World world) {
+        final WorldServer worldServer = ((CraftWorld) world).getHandle();
+        return worldServer.chunkProviderServer.forceChunkLoad;
+    }
+
+    @Override
+    public void setForceChunkLoad(final World world, final boolean force) {
+        final WorldServer worldServer = ((CraftWorld) world).getHandle();
+        worldServer.chunkProviderServer.forceChunkLoad = force;
+    }
+
+    @Override
+    public boolean isBuildable(final World world, final int x, final int y, final int z) {
+        final net.minecraft.server.v1_6_R3.World nmsWorld = ((CraftWorld) world).getHandle();
+        return nmsWorld.getMaterial(x, y, z).isBuildable();
+    }
+
+}
